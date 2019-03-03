2840ab5d0c74d829311d81ec1497193ac93b533d
Charithjayasanka <Charithjayasanka2@gmail.com>
2019-03-03 18:45:07 +0530

    java_helloworld_Charithjayasanka.java added

---
+++ scripts/java_helloworld_Charithjayasanka.java
@@ -0,0 +1,9 @@
+// LANGUAGE: Java-8
+// AUTHOR: Charith Jayasanka
+// GITHUB: https://github.com/charithjayasanka
+
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello, HactoberFest!");
+  }
+}
